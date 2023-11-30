d56c28469ee477237aded6b361211f4bd38a5fd5
quangnhph22872 <quangnhph22872@fpteduvn>
2023-11-30 15:48:43 +0700

    CTGG

---
+++ src/pages/discount-program.js
@@ -0,0 +1,167 @@
+import { useCallback, useMemo, useState, useEffect } from "react";
+import axios from "axios";
+import Head from "next/head";
+import PlusIcon from "@heroicons/react/24/solid/PlusIcon";
+import { Box, Button, Container, Link, Stack, SvgIcon, Typography } from "@mui/material";
+import { useSelection } from "src/hooks/use-selection";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import { DiscountProgramTable } from "src/sections/discountProgram/discount-program-table";
+import { DiscountProgramSearch } from "src/sections/discountProgram/discount-program-search";
+import { applyPagination } from "src/utils/apply-pagination";
+import MyPagination from "src/components/Pagination";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { SideNavItem } from "src/layouts/dashboard/side-nav-item";
+import { usePathname } from "next/navigation";
+
+const useAccount = (data, page, rowsPerPage) => {
+  return useMemo(() => {
+    console.log("data : ", data);
+    return applyPagination(data, page, rowsPerPage);
+  }, [data, page, rowsPerPage]);
+};
+
+const useAccountIds = (account) => {
+  return useMemo(() => {
+    return account.map((account) => account.id);
+  }, [account]);
+};
+
+const Page = () => {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [dataChange, setDataChange] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const account = useAccount(data, page, rowsPerPage);
+  const accountIds = useAccountIds(account);
+  const accountSelection = useSelection(accountIds);
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const [textSearch, setTextSearch] = useState("");
+
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalElements, setTotalElements] = useState(0);
+
+  const pathname = usePathname();
+
+  const item = {
+    title: "Add",
+    path: "/discount-program/add",
+    icon: (
+      <SvgIcon fontSize="small">
+        <PlusIcon />
+      </SvgIcon>
+    ),
+  };
+  const active = item.path ? pathname === item.path : false;
+
+  // Delete
+  const handleDelete = async (id) => {
+    try {
+    //   await axios.delete(`http://localhost:2003/api/admin/account/delete/${id}`);
+      console.log(id);
+      setDataChange(!dataChange);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  // Load
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const accessToken = localStorage.getItem("accessToken"); // Lấy access token từ localStorage
+        console.log(accessToken);
+        axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`; // Thêm access token vào tiêu đề "Authorization"
+
+        let Api = `http://localhost:2003/api/admin/account/loadAndSearch?current_page=${pageNumber}`;
+
+        if (textSearch !== "") {
+          Api = Api + `&key=${textSearch}`;
+        }
+        console.warn(Api);
+        const response = await axios.get(Api); // Thay đổi URL API của bạn tại đây
+        console.log(response.data);
+        setTotalPages(response.data.totalPages);
+        setTotalElements(response.data.totalElements);
+        setData(response.data.content);
+      } catch (error) {
+        if (error.response) {
+          // Xử lý response lỗi
+          if (error.response.status === 403) {
+            alert("Bạn không có quyền truy cập vào trang này");
+            window.location.href = "/"; // Thay đổi "/dang-nhap" bằng đường dẫn đến trang đăng nhập của bạn
+          } else {
+            alert("Có lỗi xảy ra trong quá trình gọi API");
+          }
+        } else {
+          console.log("Không thể kết nối đến API");
+        }
+      }
+    };
+
+    fetchData();
+  }, [pageNumber, dataChange, textSearch]);
+
+  return (
+    <>
+      <Head>
+        <title>Chương trình giảm giá |  Armani Hotel</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack direction="row" justifyContent="space-between" spacing={4}>
+              <Stack spacing={1}>
+                <Typography variant="h4">Chương trình giảm giá</Typography>
+                <Stack alignItems="center" direction="row" spacing={1}></Stack>
+              </Stack>
+              <div>
+                <SideNavItem
+                  style={{ backgroundColor: "red" }}
+                  active={active}
+                  disabled={item.disabled}
+                  external={item.external}
+                  icon={item.icon}
+                  key={item.title}
+                  path={item.path}
+                  title={item.title}
+                  btn={true}
+                />
+              </div>
+            </Stack>
+            <DiscountProgramSearch textSearch={textSearch} setTextSearch={setTextSearch} />
+            <div style={{ minHeight: 500 }}>
+              {" "}
+              <DiscountProgramTable
+                items={account}
+                selected={accountSelection.selected}
+                onDelete={handleDelete} // Thêm prop onDelete và truyền giá trị của handleDelete vào đây
+                setPageNumber={setPageNumber}
+                totalElements={totalElements}
+                pageNumber={pageNumber}
+              />
+            </div>
+          </Stack>
+
+          <MyPagination
+            pageNumber={pageNumber}
+            totalPages={totalPages}
+            setPageNumber={setPageNumber}
+          />
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
